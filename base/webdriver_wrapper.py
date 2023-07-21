027eb7e8b3e96a854778db53c95a69504811f668
Balaji <balaji0017@gmail.com>
2023-07-21 14:36:54 +0530

    Added Browser Configuration using inheritance

---
+++ base/webdriver_wrapper.py
@@ -0,0 +1,15 @@
+import pytest
+from selenium import webdriver
+
+
+class WebDriverListner:
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self):
+        # Test Setup
+        self.driver = webdriver.Edge()
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(10)
+        self.driver.get("https://demo.openemr.io/b/openemr")
+        yield
+        # Test Teardown
+        self.driver.quit()
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,25 +1,29 @@
 import pytest
 from assertpy import assert_that
-from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
+from base.webdriver_wrapper import WebDriverListner
 
 
-class TestLoginUserInterface:
-    @pytest.fixture(scope="function",autouse=True)
-    def setup(self):
-        #Test Setup
-        self.driver = webdriver.Chrome()
-        self.driver.maximize_window()
-        self.driver.implicitly_wait(10)
-        self.driver.get("https://demo.openemr.io/b/openemr")
-        yield
-        #Test Teardown
-        self.driver.quit()
-
+class TestLoginUserInterface(WebDriverListner):
     def test_title(self):
         actual_title = self.driver.title
         assert_that(actual_title).is_equal_to("OpenEMR Login")
 
     def test_application_description(self):
-        actual_desc = self.driver.find_element(By.XPATH,"//p[contains(text(),'most')]").text
+        actual_desc = self.driver.find_element(By.XPATH, "//p[contains(text(),'most')]").text
         assert_that(actual_desc).contains("Electronic Health Record")
+
+
+class TestLogin(WebDriverListner):
+
+    def test_valid_login(self):
+        self.driver.find_element(By.ID, "authUser").send_keys("admin")
+        self.driver.find_element(By.ID,"clearPass").send_keys("pass")
+        select_language=Select(self.driver.find_element(By.NAME,"languageChoice"))
+        select_language.select_by_visible_text("English (Indian)")
+        self.driver.find_element(By.ID,"login-button").click()
+        assert_that(self.driver.title).is_equal_to("OpenEMR")
+
+
